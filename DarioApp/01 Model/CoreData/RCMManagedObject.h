a969ec17fec0a1748a4ea52fce920c25ced81add
Ricardo Caballero <rcabamo@gmail.com>
2014-01-26 16:58:10 +0100

    ADD CoreData base class

---
+++ DarioApp/01 Model/CoreData/RCMManagedObject.h
@@ -0,0 +1,14 @@
+//
+//  WOOManagedObject.h
+//  DarioApp
+//
+//  Created by Ricardo Caballero on 31/10/13.
+//  Copyright (c) 2013 Ricardo Caballero. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+
+@interface RCMManagedObject : NSManagedObject
+
+@end
---
+++ DarioApp/01 Model/CoreData/RCMManagedObject.m
@@ -0,0 +1,20 @@
+//
+//  WOOManagedObject.m
+//  DarioApp
+//
+//  Created by Ricardo Caballero on 31/10/13.
+//  Copyright (c) 2013 Ricardo Caballero. All rights reserved.
+//
+
+#import "RCMManagedObject.h"
+
+@implementation RCMManagedObject
+
+- (void)awakeFromInsert
+{
+    if ([self respondsToSelector:@selector(setCreatedAt:)]) {
+        [self setValue:[NSDate date] forKey:@"createdAt"];
+    }
+}
+
+@end
