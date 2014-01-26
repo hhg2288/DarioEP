9a31077b213ab9075c6218af7aac80aa2c4aa5df
Ricardo Caballero <rcabamo@gmail.com>
2014-01-26 16:54:20 +0100

    ADD API client class

---
+++ DarioApp/01 Model/Networking/RCMAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  RCMAPIClient.h
+//  DarioApp
+//
+//  Created by Ricardo Caballero on 26/01/14.
+//  Copyright (c) 2014 Horacio Herrera. All rights reserved.
+//
+
+typedef void (^RCMRequestCompletionBlock)(NSArray *response, NSError *error);
+
+@interface RCMAPIClient : NSObject
+
+@end
---
+++ DarioApp/01 Model/Networking/RCMAPIClient.m
@@ -0,0 +1,15 @@
+//
+//  RCMAPIClient.m
+//  DarioApp
+//
+//  Created by Ricardo Caballero on 26/01/14.
+//  Copyright (c) 2014 Horacio Herrera. All rights reserved.
+//
+
+#import "RCMAPIClient.h"
+
+#import <Backbeam/Backbeam.h>
+
+@implementation RCMAPIClient
+
+@end
